added finetuning validation fixed neus1 model added options for frustum sampling and input space rotation

models.py
@@ -985,9 +985,9 @@ class Renderer_Neus_Foreground1(nn.Module):
         self.d_sdf_feature = 128
         self.d_normals = input_ch
 
-        self.sdf_network = SDFNetwork(input_ch, input_ch_views, input_ch_feat, self.d_sdf_feature+1)
-        self.rendering_network = RenderingNetwork(input_ch, self.d_normals, input_ch_views, input_ch_feat, self.d_sdf_feature)
-        self.deviation_network = SingleVarianceNetwork(0.3)
+        self.sdf_network = SDFNetwork1(input_ch, input_ch_views, input_ch_feat, self.d_sdf_feature+1)
+        self.rendering_network = RenderingNetwork1(input_ch, self.d_normals, input_ch_views, input_ch_feat, self.d_sdf_feature)
+        self.deviation_network = SingleVarianceNetwork1(0.3)
 
     def forward_alpha(self,x):
         return None
@@ -1053,7 +1053,7 @@ class SDFNetwork1(nn.Module):
         self.num_layers = len(dims)
         self.skip_in = skip_in
         self.scale = scale
-        self.d_bottleneck_feat = 10
+        self.d_bottleneck_feat = 64
 
         for l in range(0, self.num_layers - 1):
             if l + 1 in self.skip_in:
@@ -1163,6 +1163,8 @@ class RenderingNetwork1(nn.Module):
         self.squeeze_out = squeeze_out
         dims = [d_in + d_in_normals + d_in_views + d_in_mvs_feat + d_in_sdf_feat] + [d_hidden for _ in range(n_layers)] + [d_out]
 
+        self.d_bottleneck_feat = 64
+
         self.embednorms_fn = None
         if multires_norms > 0:
             embednorms_fn, input_ch = get_embedder(multires_norms)
@@ -1187,7 +1189,7 @@ class RenderingNetwork1(nn.Module):
                 setattr(self, "bias_lin" + str(l), pts_bias)
 
         self.relu = nn.ReLU()
-        self.pts_bias = nn.Linear(d_in_mvs_feat + d_in_sdf_feat + d_in_normals + d_in_views, d_hidden)
+        self.pts_bias = nn.Linear(d_in_mvs_feat + d_in_sdf_feat + d_in_normals + d_in_views, self.d_bottleneck_feat)
 
     def forward(self, points, normals, view_dirs, mvs_feature_vector, sdf_feature_vector):
         if self.embednorms_fn is not None:
@@ -1197,13 +1199,13 @@ class RenderingNetwork1(nn.Module):
 
         x = rendering_input
         bias = self.pts_bias(torch.cat([view_dirs, normals, mvs_feature_vector, sdf_feature_vector], dim=-1))
-        bias = self.activation(bias)
+        bias = self.relu(bias)
         for l in range(0, self.num_layers - 1):
             lin = getattr(self, "lin" + str(l))
 
             if(l > 0):
                 bias_lin = getattr(self, "bias_lin" + str(l))
-                x = x + self.activation(bias_lin(bias))
+                x = x + self.relu(bias_lin(bias))
 
             x = lin(x)
 
@@ -1608,7 +1610,7 @@ class MVSNeRF(nn.Module):
         sdf = self.nerf.sdf(x)
         return sdf
 
-def create_nerf_mvs(args, pts_embedder=True, use_mvs=False, dir_embedder=True):
+def create_nerf_mvs(args, pts_embedder=True, use_mvs=False, dir_embedder=True, finetuning=False):
     """Instantiate mvs NeRF's MLP model.
     """
 
@@ -1647,7 +1649,8 @@ def create_nerf_mvs(args, pts_embedder=True, use_mvs=False, dir_embedder=True):
     EncodingNet = None
     if use_mvs:
         EncodingNet = MVSNet().to(device)
-        grad_vars += list(EncodingNet.parameters())    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if not finetuning:
+            grad_vars += list(EncodingNet.parameters())    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     start = 0
 

renderer.py
@@ -253,8 +253,8 @@ def rendering(args, pose_ref, rays_pts, rays_pts_ndc, depth_candidates, rays_o, 
         H, W = int(H), int(W)
         inv_scale = torch.tensor([W-1, H-1]).cuda()
         
-        input_pts = world_to_sdf_input_space(pose_ref, rays_pts, inv_scale)
-        input_dir = world_to_sdf_input_space_dirs(pose_ref, rays_dir[:,None,:], inv_scale).squeeze(1)
+        input_pts = world_to_sdf_input_space(pose_ref, rays_pts, args, inv_scale)
+        input_dir = world_to_sdf_input_space_dirs(pose_ref, rays_dir[:,None,:], args, inv_scale).squeeze(1)
 
         pts_norm = torch.linalg.norm(rays_pts, ord=2, dim=-1)
         inside_sphere = (pts_norm < 1.0).float().detach()
@@ -304,7 +304,7 @@ def mesh_rendering(args, pose_ref, rays_pts, rays_pts_ndc, inv_scale,
     H, W = int(H), int(W)
     inv_scale = torch.tensor([W-1, H-1]).cuda()
     
-    input_pts = world_to_sdf_input_space(pose_ref, rays_pts, inv_scale)
+    input_pts = world_to_sdf_input_space(pose_ref, rays_pts, args, inv_scale)
     input_dir = input_pts # dirs are not used for sdf and their values do not matter
 
     if 'neus' in args.net_type:

utils.py
@@ -862,20 +862,22 @@ def rotate_2d(axis, points, angle):
 
     return points
 
-def world_to_sdf_input_space(pose_ref, point_samples, inv_scale=None):
+def world_to_sdf_input_space(pose_ref, point_samples, args, inv_scale=None):
     ref_idx = -1
     pad = 24
     # points = points_w2c_unitspshere(pose_ref, point_samples, inv_scale, ref_idx)
     # points = get_ndc_coordinate(pose_ref['w2cs'][ref_idx], pose_ref['intrinsics'][ref_idx], point_samples, inv_scale, near=pose_ref['near_fars'][ref_idx][0], far=pose_ref['near_fars'][ref_idx][1], pad=pad)
     # points = rotate_unitsphere(pose_ref, point_samples, ref_idx)
-    points = rotate_unitsphere_2D(pose_ref, point_samples, ref_idx)
-    # points = point_samples
+    if args.rotate_space:
+        points = rotate_unitsphere_2D(pose_ref, point_samples, ref_idx)
+    else:
+        points = point_samples
     return points
 
-def world_to_sdf_input_space_dirs(pose_ref, point_samples, inv_scale=None): 
+def world_to_sdf_input_space_dirs(pose_ref, point_samples, args, inv_scale=None): 
     zero_points = torch.zeros_like(point_samples)
-    dir_points = world_to_sdf_input_space(pose_ref, point_samples, inv_scale)
-    zero_points = world_to_sdf_input_space(pose_ref, zero_points, inv_scale)
+    dir_points = world_to_sdf_input_space(pose_ref, point_samples, args, inv_scale)
+    zero_points = world_to_sdf_input_space(pose_ref, zero_points, args, inv_scale)
     dirs = dir_points - zero_points
 
     # dirs = dirs / (torch.sum(dirs, dim=-1, keepdim=True) + 1e-5)
@@ -886,20 +888,22 @@ def world_to_sdf_input_space_dirs(pose_ref, point_samples, inv_scale=None):
     # dirs = dirs.reshape(nr, ns, 3)
     return dirs
 
-def sdf_input_space_to_world(pose_ref, point_samples, inv_scale=None):
+def sdf_input_space_to_world(pose_ref, point_samples, args, inv_scale=None):
     ref_idx = -1
     pad = 24
     # points = points_c2w_unitspshere(pose_ref, point_samples, inv_scale, ref_idx)
     # points = inverse_get_ndc_coordinate(pose_ref['c2ws'][ref_idx], pose_ref['intrinsics'][ref_idx], point_samples, inv_scale, near=pose_ref['near_fars'][ref_idx][0], far=pose_ref['near_fars'][ref_idx][1], pad=pad)
     # points = inverse_rotate_unitsphere(pose_ref, point_samples, ref_idx)
-    points = inverse_rotate_unitsphere_2D(pose_ref, point_samples, ref_idx)
-    # points = point_samples
+    if args.rotate_space:
+        points = inverse_rotate_unitsphere_2D(pose_ref, point_samples, ref_idx)
+    else:
+        points = point_samples
     return points
 
-def sdf_input_space_to_world_dirs(pose_ref, point_samples, inv_scale=None):
+def sdf_input_space_to_world_dirs(pose_ref, point_samples, args, inv_scale=None):
     zero_points = torch.zeros_like(point_samples)
-    dir_points = sdf_input_space_to_world(pose_ref, point_samples, inv_scale)
-    zero_points = sdf_input_space_to_world(pose_ref, zero_points, inv_scale)
+    dir_points = sdf_input_space_to_world(pose_ref, point_samples, args, inv_scale)
+    zero_points = sdf_input_space_to_world(pose_ref, zero_points, args, inv_scale)
     dirs = dir_points - zero_points
 
     # dirs = dirs / (torch.sum(dirs, dim=-1, keepdim=True) + 1e-5)
@@ -971,13 +975,19 @@ def near_far_from_sphere(rays_o, rays_d):
     far = mid + 1.0
     return near, far
 
-def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target, args, perturb, sdf_network, network_query_fn, n_samples=64, n_outside=0, n_importance=64, up_sample_steps=4):
+def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target, args, perturb, sdf_network, network_query_fn, n_samples=64, n_outside=32, n_importance=32, up_sample_steps=4):
     batch_size, _ = rays_o.shape
     N, V, C, H, W = imgs.shape
     inv_scale = torch.tensor([W-1, H-1]).cuda()
-    # near, far = near_far_from_sphere(rays_o, rays_d)
+    near, far = near_far_from_sphere(rays_o, rays_d)
+    if args.frustum_sampling:
+        near, far = near_far_target
+        n_samples = 64
+        n_importance = 64
+        n_outside = 0
+        up_sample_steps = 4
     #sample_dist = 2.0 / n_samples   # Assuming the region of interest is a unit sphere
-    near, far = near_far_target
+    # near, far = ear_far_target
     # z_vals = torch.linspace(0.0, 1.0, n_samples).cuda()
     # z_vals = near.cuda() + (far.cuda() - near.cuda()) * z_vals[None, :].cuda()
 
@@ -1018,7 +1028,7 @@ def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target
     if n_importance > 0:
         with torch.no_grad():
             pts_world = rays_o[:, None, :] + rays_d[:, None, :] * z_vals[..., :, None]
-            pts = world_to_sdf_input_space(pose_ref, pts_world, inv_scale)
+            pts = world_to_sdf_input_space(pose_ref, pts_world, args, inv_scale)
 
             pts_norm = torch.linalg.norm(pts, ord=2, dim=-1)
             inside_sphere = pts_norm < 1.0
@@ -1035,7 +1045,7 @@ def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target
 
             # input views are not used in sdf -> pass anything as input views
             sdf = network_query_fn(pts, pts, input_feat, sdf_network.sdf).squeeze()
-            sdf[~inside_sphere] = 100
+            #sdf[~inside_sphere] = 100
 
             for i in range(up_sample_steps):
                 new_z_vals = up_sample(rays_o,
@@ -1047,7 +1057,7 @@ def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target
                                             pts)
 
                 new_pts_world = rays_o[:, None, :] + rays_d[:, None, :] * new_z_vals[..., :, None]
-                new_pts = world_to_sdf_input_space(pose_ref, new_pts_world, inv_scale)
+                new_pts = world_to_sdf_input_space(pose_ref, new_pts_world, args, inv_scale)
 
                 input_feat = gen_pts_feats_no_ndc(imgs, volume_feature, new_pts_world,
                                                     pose_ref,
@@ -1071,7 +1081,7 @@ def gen_pts_neus(rays_o, rays_d, imgs, volume_feature, pose_ref, near_far_target
                                          last=(i + 1 == up_sample_steps))
 
                 pts_world = rays_o[:, None, :] + rays_d[:, None, :] * z_vals[..., :, None]
-                pts = world_to_sdf_input_space(pose_ref, pts_world, inv_scale)
+                pts = world_to_sdf_input_space(pose_ref, pts_world, args, inv_scale)
                 pts_ndc = get_ndc_points(pose_ref, pts_world, imgs, ref_idx=0, pad=args.pad)
 
     # Background model

val_mvs_neus.py
@@ -71,11 +71,21 @@ psnr_all,ssim_all,LPIPS_vgg_all = [],[],[]
 depth_acc = {}
 eval_metric = [0.1,0.05,0.01]
 depth_acc[f'abs_err'],depth_acc[f'acc_l_{eval_metric[0]}'],depth_acc[f'acc_l_{eval_metric[1]}'],depth_acc[f'acc_l_{eval_metric[2]}'] = {},{},{},{}
-for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
+
+create_mesh = True
+is_finetuned = False
+scene = 1
+
+enum = enumerate([1,8,21,103,114])
+
+if is_finetuned:
+    enum = enumerate([scene])
+
+for i_scene, scene in enum:#,8,21,103,114
     psnr,ssim,LPIPS_vgg,rgbs = [],[],[],[]
     cmd = f'--datadir /abyss/home/data/mvsnerf_dtu/mvs_training/dtu/scan{scene}  \
      --dataset_name dtu_ft  \
-     --net_type v0 --ckpt ./ckpts//mvsnerf-v0.tar '
+     --net_type v0 --ckpt ./ckpts//latest.tar '
 
     args = config_parser(cmd.split())
     args.use_viewdirs = True
@@ -88,13 +98,16 @@ for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
         render_kwargs_train, render_kwargs_test, start, grad_vars = create_nerf_mvs(args, use_mvs=True, dir_embedder=False, pts_embedder=True)
         filter_keys(render_kwargs_train)
 
+        pytorch_total_params_fn = sum(p.numel() for p in render_kwargs_train['network_fn'].parameters() if p.requires_grad)
+        pytorch_total_params_mvs = sum(p.numel() for p in render_kwargs_train['network_mvs'].parameters() if p.requires_grad)
+
         MVSNet = render_kwargs_train['network_mvs']
         render_kwargs_train.pop('network_mvs')
 
 
     datadir = args.datadir
     datatype = 'train'
-    pad = 16
+    pad = 24
     args.chunk = 5120
 
 
@@ -102,13 +115,16 @@ for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
     dataset_train = dataset_dict[args.dataset_name](args, split='train')
     dataset_val = dataset_dict[args.dataset_name](args, split='val')
     val_idx = dataset_val.img_idx
+
+    if 0==i_scene:
+        print('params_fn: ', pytorch_total_params_fn)
+        print('params_mvs: ', pytorch_total_params_mvs)
     
     save_as_image = True
     save_dir = f'results/test3'
     os.makedirs(save_dir, exist_ok=True)
     MVSNet.train()
     MVSNet = MVSNet.cuda()
-    
 
 
     with torch.no_grad():
@@ -117,6 +133,88 @@ for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
             tqdm._instances.clear() 
         except Exception:     
             pass
+
+        if create_mesh:
+            for i, batch in enumerate(tqdm(dataset_val)):
+                torch.cuda.empty_cache()
+                
+                rays, img = decode_batch(batch)
+                rays = rays.squeeze().to(device)  # (H*W, 3)
+                img = img.squeeze().cpu().numpy()  # (H, W, 3)
+                depth = batch['depth'].squeeze().numpy()  # (H, W)
+            
+                # find nearest image idx from training views
+                positions = dataset_train.poses[:,:3,3]
+                dis = np.sum(np.abs(positions - dataset_val.poses[[i],:3,3]), axis=-1)
+                pair_idx = np.argsort(dis)[:3]
+                pair_idx = [dataset_train.img_idx[item] for item in pair_idx]
+                
+                imgs_source, proj_mats, near_far_source, pose_source = dataset_train.read_source_views(pair_idx=pair_idx,device=device)
+                volume_feature, _, _ = MVSNet(imgs_source, proj_mats, near_far_source, pad=pad)
+                imgs_source = unpreprocess(imgs_source)
+
+                if is_finetuned:   
+                    imgs_source, proj_mats, near_far_source, pose_source = dataset_train.read_source_views(device=device)
+                    imgs_source = unpreprocess(imgs_source)
+                    volume_feature = torch.load(args.ckpt)['volume']['feat_volume']
+                    volume_feature = RefVolume(volume_feature.detach()).cuda()
+                    pad = 24
+
+                # create 3D mesh
+                sdf_all = []
+                near = -1
+                far = 1
+                resolution = 200
+                use_alpha = True
+
+                if 'neus' in args.net_type:
+                    threshold = 0.5
+                    if(not use_alpha):
+                        threshold = 0
+                else:
+                    threshold = 0.5
+
+                pts, rays_o, rays_d, z_vals = point_grid([near,near,near],[far,far,far],resolution)
+                pts, rays_o, rays_d, z_vals = pts.to(device), rays_o.to(device), rays_d.to(device), z_vals.to(device)
+                xyz_coarse_sampled = pts
+
+                # Converting world coordinate to ndc coordinate
+                H, W = img.shape[:2]
+                inv_scale = torch.tensor([W - 1, H - 1]).to(device)
+                w2c_ref, intrinsic_ref = pose_source['w2cs'][0], pose_source['intrinsics'][0].clone()
+                xyz_NDC = get_ndc_coordinate(w2c_ref, intrinsic_ref, xyz_coarse_sampled, inv_scale,
+                                                near=near_far_source[0], far=near_far_source[1], pad=pad*args.imgScale_test)
+
+                # rendering
+                sdf = mesh_rendering(args, pose_source, xyz_coarse_sampled,
+                                        xyz_NDC, inv_scale,
+                                        volume_feature, imgs_source, **render_kwargs_train)
+
+                cos_anneal_ratio = 1.0
+
+                if use_alpha:
+                    # rendering
+                    rgb, disp, acc, depth_pred, alpha, extras, _, _, _ = rendering(args, pose_source, xyz_coarse_sampled,
+                                                                            xyz_NDC, z_vals, rays_o, rays_d, inv_scale, cos_anneal_ratio,
+                                                                            volume_feature,imgs_source, **render_kwargs_train)
+
+                    sdf = 1 - alpha
+
+
+                
+                pts_norm = torch.linalg.norm(pts, ord=2, dim=-1)
+                outside_sphere = (pts_norm > 1.0).bool()
+
+                # outside_frame, _ = torch.max(xyz_NDC,dim=-1)
+                # outside_frame = outside_frame > 1
+
+                sdf[outside_sphere] = 1.0
+
+                sdf = sdf.reshape([resolution,resolution,resolution])
+                sdf = sdf.cpu().detach().numpy()
+                validate_mesh(save_dir, sdf, i_scene*1000 + i, world_space=False,
+                            resolution=resolution, threshold=threshold)
+                #3D mesh done
 
         for i, batch in enumerate(tqdm(dataset_val)):
             torch.cuda.empty_cache()
@@ -136,60 +234,12 @@ for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
             volume_feature, _, _ = MVSNet(imgs_source, proj_mats, near_far_source, pad=pad)
             imgs_source = unpreprocess(imgs_source)
 
-            # create 3D mesh
-            sdf_all = []
-            near = -1
-            far = 1
-            resolution = 100
-            if 'neus' in args.net_type:
-                threshold = 0.5
-            else:
-                threshold = 0.5
-            use_alpha = True
-            if(not use_alpha):
-                threshold = 0
-            pts, rays_o, rays_d, z_vals = point_grid([near,near,near],[far,far,far],resolution)
-            pts, rays_o, rays_d, z_vals = pts.to(device), rays_o.to(device), rays_d.to(device), z_vals.to(device)
-            xyz_coarse_sampled = pts
-
-            # Converting world coordinate to ndc coordinate
-            H, W = img.shape[:2]
-            inv_scale = torch.tensor([W - 1, H - 1]).to(device)
-            w2c_ref, intrinsic_ref = pose_source['w2cs'][0], pose_source['intrinsics'][0].clone()
-            xyz_NDC = get_ndc_coordinate(w2c_ref, intrinsic_ref, xyz_coarse_sampled, inv_scale,
-                                            near=near_far_source[0], far=near_far_source[1], pad=pad*args.imgScale_test)
-
-            # rendering
-            sdf = mesh_rendering(args, pose_source, xyz_coarse_sampled,
-                                    xyz_NDC, inv_scale,
-                                    volume_feature, imgs_source, **render_kwargs_train)
-
-            cos_anneal_ratio = 1.0
-
-            if use_alpha:
-                # rendering
-                rgb, disp, acc, depth_pred, alpha, extras, _, _, _ = rendering(args, pose_source, xyz_coarse_sampled,
-                                                                        xyz_NDC, z_vals, rays_o, rays_d, inv_scale, cos_anneal_ratio,
-                                                                        volume_feature,imgs_source, **render_kwargs_train)
-
-                sdf = alpha
-
-
-            
-            # pts_norm = torch.linalg.norm(pts, ord=2, dim=-1)
-            # outside_sphere = (pts_norm > 1.0).bool()
-
-            # outside_frame, _ = torch.max(xyz_NDC,dim=-1)
-            # outside_frame = outside_frame > 1
-
-            # sdf[outside_frame] = 1.0
-
-            sdf = sdf.reshape([resolution,resolution,resolution])
-            sdf = sdf.cpu().detach().numpy()
-            validate_mesh(save_dir, sdf, i_scene*1000 + i, world_space=False,
-                          resolution=resolution, threshold=threshold)
-            #3D mesh done
-
+            if is_finetuned:   
+                imgs_source, proj_mats, near_far_source, pose_source = dataset_train.read_source_views(device=device)
+                imgs_source = unpreprocess(imgs_source)
+                volume_feature = torch.load(args.ckpt)['volume']['feat_volume']
+                volume_feature = RefVolume(volume_feature.detach()).cuda()
+                pad = 24
         
             N_rays_all = rays.shape[0]
             rgb_rays, depth_rays_preds = [],[]
@@ -197,6 +247,11 @@ for i_scene, scene in enumerate([1,8,21,103,114]):#,8,21,103,114
 
                 xyz_coarse_sampled, rays_o, rays_d, z_vals = ray_marcher(rays[chunk_idx*args.chunk:(chunk_idx+1)*args.chunk],
                                                     N_samples=args.N_samples)
+
+                if('neus' in args.net_type and args.neus_sampling):
+                    xyz_coarse_sampled, z_vals, _ = gen_pts_neus(
+                        rays_o, rays_d, imgs_source, volume_feature, pose_source, near_far_source, args, False, render_kwargs_train["network_fn"], render_kwargs_train["network_query_fn"])
+
 
 
                 # Converting world coordinate to ndc coordinate